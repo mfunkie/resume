0d99a20c13de68fb20634000e1b4590c0fb87e8e
Mark Funk <mark@boomtownroi.com>
2014-12-31 12:59:46 -0500

    Copy

--- src/scripts/components/ExperienceBoomTown.jsx
+++ src/scripts/components/ExperienceBoomTown.jsx
@@ -17,7 +17,7 @@ var boomTownItems = [
 'Tuned existing features in the CRM for front-end performance. ' +
   'This included lowering loading times for the top used feature in the CRM by up to 80%.',
 'Provided mentorship and training on Javascript and Angular development for the CRM team.',
-'Continually researched and implemented new techniques and best practices for UI development and performance',
+'Continually researched and implemented new techniques and best practices for UI development and performance.',
 'Engineered several re-usable components and services to make future projects easier to build.',
 'Developed and maintained the build tools for all Angular features in the CRM.',
 'Worked on a Lead Integration tool that consumes emails from third parties and creates Leads in the database.'
